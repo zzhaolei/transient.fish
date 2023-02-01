aae909782247d55b046eb5633e75022bb63d09f6
zhaolei <im.zhaolei@foxmail.com>
2023-02-01 11:01:01 +0800

    support fish_right_prompt transient

--- conf.d/transient.fish
+++ conf.d/transient.fish
@@ -1,61 +1,91 @@
-if not type --query __fish_prompt
-    functions --copy fish_prompt __fish_prompt
+# rename fish_prompt to __transient_fish_prompt
+if not type --query __transient_fish_prompt
+    and type --query fish_prompt
+    functions --copy fish_prompt __transient_fish_prompt
     functions --erase fish_prompt
 end
 
-function __default_transient_prompt
+# rename fish_right_prompt to __transient_fish_right_prompt
+if not type --query __transient_fish_right_prompt
+    and type --query fish_right_prompt
+    functions --copy fish_right_prompt __transient_fish_right_prompt
+    functions --erase fish_right_prompt
+end
+
+function ___default_transient_prompt
     printf (set_color 5FD700)"❯ "(set_color normal)
 end
 
-set --global TRANSIENT 0
+function ___reset_pipestatus
+    # Use the variables defined in the calling function. (_transient_pipestatus)
+    return $_transient_pipestatus
+end
+
+set --global TRANSIENT normal
+set --global TRANSIENT_RIGHT normal
 
 function fish_prompt
-    _transient_pipestatus=$pipestatus _transient_status=$status if test "$TRANSIENT" = 0
-        function __raise_pipestatus
-            return $_transient_pipestatus
-        end
-        if type --query __fish_prompt
-            # reset command status to last status
-            __raise_pipestatus
-            __fish_prompt
+    _transient_pipestatus=$pipestatus _transient_status=$status if test "$TRANSIENT" = normal
+        if type --query __transient_fish_prompt
+            ___reset_pipestatus
+            __transient_fish_prompt
         else
-            __default_transient_prompt
+            ___default_transient_prompt
         end
     else
         printf \e\[0J # clear from cursor to end of screen
         if type --query transient_prompt_func
             transient_prompt_func
         else
-            __default_transient_prompt
+            ___default_transient_prompt
         end
 
-        if test "$TRANSIENT" = 2
-            set --global TRANSIENT 0
+        if test "$TRANSIENT" = transient-repaint
+            set --global TRANSIENT normal
+            set --global TRANSIENT_RIGHT transient-repaint
             commandline --function repaint
+        end
+    end
+end
+
+function fish_right_prompt
+    if test "$TRANSIENT_RIGHT" = transient-repaint
+        set --global TRANSIENT_RIGHT normal
+        commandline --function repaint
+        return 0
+    end
+
+    _transient_pipestatus=$pipestatus _transient_status=$status if test "$TRANSIENT_RIGHT" = normal
+        if type --query __transient_fish_right_prompt
+            ___reset_pipestatus
+            __transient_fish_right_prompt
         end
     end
 end
 
 # transience related functions
 function reset-transient --on-event fish_postexec
-    set --global TRANSIENT 0
+    set --global TRANSIENT normal
+    set --global TRANSIENT_RIGHT normal
 end
 
 function transient_execute
     if commandline --is-valid
-        set --global TRANSIENT 1
+        set --global TRANSIENT transient
+        set --global TRANSIENT_RIGHT transient
         commandline --function repaint
     else if test "$(commandline -b)" = "" # fix empty enter
-        set --global TRANSIENT 2
+        set --global TRANSIENT transient-repaint
+        set --global TRANSIENT_RIGHT transient
         commandline --function repaint
     else
-        set --global TRANSIENT 0
+        set --global TRANSIENT normal
     end
     commandline --function execute
 end
 
 function ctrl_c_transient_execute
-    set --global TRANSIENT 2
+    set --global TRANSIENT transient-repaint
     if test "$(commandline -b)" = ""
         commandline --function repaint execute
         return 0
