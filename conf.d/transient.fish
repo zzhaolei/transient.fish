70db06be0c669c6e4973d071c5441739e1154874
zhaolei <im.zhaolei@foxmail.com>
2023-02-09 14:19:15 +0800

    improve fisher uninstall event

--- conf.d/transient.fish
+++ conf.d/transient.fish
@@ -107,3 +107,25 @@ bind --user --mode default \r __transient_execute
 bind --user --mode insert \r __transient_execute
 bind --user --mode default \cc __transient_ctrl_c_execute
 bind --user --mode insert \cc __transient_ctrl_c_execute
+
+function __transient_uninstall --on-event transient_uninstall
+    if type --query __transient_fish_mode_prompt
+        functions --erase fish_mode_prompt
+        functions --copy __transient_fish_mode_prompt fish_mode_prompt
+    end
+
+    if type --query __transient_fish_prompt
+        functions --erase fish_prompt
+        functions --copy __transient_fish_prompt fish_prompt
+    end
+
+    if type --query __transient_fish_right_prompt
+        functions --erase fish_right_prompt
+        functions --copy __transient_fish_right_prompt fish_right_prompt
+    end
+
+    functions --erase (functions --all | string match --entire -r '^__transient')
+
+    bind --user | string replace --filter --regex -- "bind (.+)( '?__transient.*)" 'bind -e $1' | source
+    set --names | string replace --ignore-case --filter --regex -- '(^transient.*)' 'set -e $1' | source
+end
