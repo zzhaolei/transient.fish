e015d07a9fbdf587263c08f5f7759ca6e348bc12
zhaolei <im.zhaolei@foxmail.com>
2024-04-19 17:53:04 +0800

    fix: entering transient prompt when typing '\' and 'enter' key

--- functions/__transient.fish
+++ functions/__transient.fish
@@ -1,7 +1,17 @@
 function __transient
     function __transient_execute
+        # --is-valid:
+        #     Returns true when the commandline is syntactically valid and complete.
+        #     If it is, it would be executed when the execute bind function is called. If the commandline is incomplete, return 2, if erroneus, return 1.
+        #
+        #     - 0: valid and complete
+        #     - 1: handle type "\[enter]"
+        #     - 2: The empty commandline is an error, not incomplete
         commandline --is-valid
-        if test $status -eq 2 # The empty commandline is an error, not incomplete
+        set -l valid $status
+        if test $valid -eq 1
+            and test "$(commandline --current-buffer)" = "\\"
+            or test $valid -eq 2
             or commandline --paging-full-mode
             commandline -f execute
             return 0
